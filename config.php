9c7a34692ddd517ba75dcc820df65d9617c17a06
David Eddy <me@davidjeddy.com>
2013-12-16 02:32:46 -0500

    Initial commit of repo.

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/* Standard configs */
+define('SITENANE', "David Eddy's Portfolio");
+define('YEAR', 2014);
+
+/* Edit for your locality */
+define('DTFORMATE', "YYYY-MM-DD");
+define('TIMEFORMATE', "HH-mm-ss");
+
+/* DO NOT CHANGE! */
+define('SITEROOT', "./");
